0116cf1697f5c9d3d2280627e64828973346ba9f
mikeykh <mharris1238@students.mchenry.edu>
2017-03-01 13:01:43 -0600

    Add files via upload

---
+++ Read File.py
@@ -0,0 +1,28 @@
+# Download the file names.txt
+# Save it into the same project folder in pycharm
+# that you are using for this project.
+# Your Python file will:
+# Read and Display the list of names from the file
+# Display the number of names that are read from the file
+
+# (You will need a variable to keep a count of
+# the number of items read from the file.)
+# Use functions and variables with appropriate names and comments.
+# Upload the .py file to GitHub and hand in the link.
+
+
+def main():
+    # This function reads a file and
+    # calculates the amount of numbers within it.
+    # Also, it prints the content of the file and
+    # prints the calculated amount.
+    number = 0
+    names_file = open("names.txt", "r")
+    for count in range(1, 23):
+        number += 1
+    print(names_file.read())
+    names_file.close()
+    print("The number of names in this list is: ", number)
+
+
+main()
