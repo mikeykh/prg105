624770c56fa147d7812709758ff285544372e457
mikeykh <mharris1238@students.mchenry.edu>
2017-03-01 13:02:41 -0600

    Add files via upload

---
+++ Average of Numbers.py
@@ -0,0 +1,27 @@
+# Write a program that uses the numbers.txt file,
+# which contains a series of integers.
+# Your program will calculate the average of all of the numbers
+# stored in the file and display the total on the screen.
+# Use appropriate variables, functions, and comments.
+# Upload your .py file to GitHub and hand in the link.
+
+
+def main():
+    # This function opens the file numbers.txt
+    # and calculates the average of all the numbers within it
+    # and prints a meaningful message.
+    divisor = 18
+    total = 0
+    average = 0
+    numbers_file = open("numbers.txt", "r")
+    for count in range(1, 19):
+        divisor += 1
+    for line in numbers_file:
+        total += (int(line))
+        average = total/18
+    print(numbers_file.read())
+    numbers_file.close()
+    print("The average of the numbers in this list is: ", average)
+
+
+main()
