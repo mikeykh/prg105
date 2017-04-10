0d29e6573b1a387a763b594db38467f300de3ec4
mikeykh <mharris1238@students.mchenry.edu>
2017-04-10 14:12:13 -0500

    Add files via upload

---
+++ 11.1/11.1 Main Program.py
@@ -0,0 +1,14 @@
+# imports the desk module
+import desk
+
+
+def main():
+    # initialize using the Desk class from the desk module
+    product_info = desk.Desk("Desk", "Balsa", "7ft", "3ft", "3ft", 699.99, "Right", 2)
+
+    # Prints the information
+    print(product_info)
+
+
+main()
+# call the main function
